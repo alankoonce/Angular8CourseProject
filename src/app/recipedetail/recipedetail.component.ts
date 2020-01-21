f5bf808e53707ac62af6f64e2a55e626cf71be04
Alan Koonce <alankoonce2@yahoo.com>
2020-01-20 21:02:33 -0700

    Add ingredients and buttons

--- src/app/recipedetail/recipedetail.component.ts
+++ src/app/recipedetail/recipedetail.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recipedetail.component.css']
 })
 export class RecipedetailComponent implements OnInit {
-
+  ingredients = [];
   constructor() {
   }
 
